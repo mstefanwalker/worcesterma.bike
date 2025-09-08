399c4a3aaf0cd84dbf6eb50d8d4b9e7a10798802
M Stefan Walker <m.stefan.walker@gmail.com>
2025-09-07 21:23:03 -0400

    Add map svg to site

--- map.py
+++ map.py
@@ -5,11 +5,13 @@
 # dependencies = [
 #     "pandas",
 #     "geopandas",
+#     "shapely",
 # ]
 # ///
 
 import pandas as pd
 import geopandas as gpd
+from shapely import get_coordinates
 
 
 def main():
@@ -17,17 +19,43 @@ def main():
     name_df = pd.read_csv('street_name_table.csv')
     geo_df = geo_df.merge(name_df, how='left', left_on='NEW_NM_ID', right_on='new_nm_id')
 
+    minx, miny, maxx, maxy = geo_df.total_bounds
+    geo_df['minx'], geo_df['miny'], geo_df['maxx'], geo_df['maxy'] = minx, miny, maxx, maxy
+    diffx = maxx - minx
+    diffy = maxy - miny
+    if diffy > diffx: geo_df['scale'] = 1 / diffy
+    else: geo_df['scale'] = 1 / diffx
+
+    geo_df['svg'] = geo_df.apply(lambda r: row_to_svg(r), axis=1)
+
     print(geo_df.head(), end="\n\n")
     print(geo_df.columns, end="\n\n")
 
-    for i, row in enumerate(geo_df.iterfeatures()):
-        if i >= 10:
-            break
-        nm_id = row['properties']['NEW_NM_ID']
-        speed = row['properties']['Speed']
-        name = row['properties']['full_name']
-        coords = row['geometry']['coordinates']
-        print(f'{nm_id} {speed:2} {name:20} {str(coords)[:50]}...')
+    svg_string = '<svg xmlns="http://www.w3.org/2000/svg" width="600" height="600" viewBox="0 0 1 1" fill="none">'
+    geo_df = geo_df.sort_values(by=['Speed'], ascending=False) # place gray roads below green roads
+    for row in geo_df.iterfeatures():
+        svg_string += row['properties']['svg'] + '\n'
+    svg_string += '</svg>'
+    with open('map.svg', 'w') as f:
+        f.write(svg_string)
+
+
+def row_to_svg(row):
+
+    line = row['geometry']
+    coords = get_coordinates(line)
+    svg_line = 'M'
+    for coord in coords:
+        x = (coord[0] - row['minx']) * row['scale']
+        y = (coord[1] - row['miny']) * row['scale']
+        y = 1 - y # flip y because 0, 0 is top left
+        svg_line += f'{x},{y} L'
+    svg_line = svg_line[:-2]
+
+    color = 'green' if row['Speed'] <= 25 else '#d8d8d8'
+    width = 0.0015 if row['Speed'] <= 25 else 0.0040
+
+    return f'<path stroke="{color}" stroke-width="{width}" d="{svg_line}"/>'
 
 
 if __name__ == "__main__":
