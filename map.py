f4d20a06bec6965b69846eb2266ddebcc0fca860
M Stefan Walker <m.stefan.walker@gmail.com>
2025-09-07 22:16:22 -0400

    De-emphasize fast roads and ramps

--- map.py
+++ map.py
@@ -15,10 +15,12 @@ from shapely import get_coordinates
 
 
 def main():
+    # load files to dataframe
     geo_df = gpd.read_file('street_centerlines.geojson')
     name_df = pd.read_csv('street_name_table.csv')
     geo_df = geo_df.merge(name_df, how='left', left_on='NEW_NM_ID', right_on='new_nm_id')
 
+    # add measurements for svg gen
     minx, miny, maxx, maxy = geo_df.total_bounds
     geo_df['minx'], geo_df['miny'], geo_df['maxx'], geo_df['maxy'] = minx, miny, maxx, maxy
     diffx = maxx - minx
@@ -32,13 +34,19 @@ def main():
         geo_df['offsetx'] = 0.5 * (diffx - diffy)
         geo_df['offsety'] = 0
 
+    # add svg
     geo_df['svg'] = geo_df.apply(lambda r: row_to_svg(r), axis=1)
 
+    # see the stuff
     print(geo_df.head(), end="\n\n")
     print(geo_df.columns, end="\n\n")
+    print(geo_df['ROADCLASS'].unique())
 
+    # sort for improved rendering
+    geo_df = geo_df.sort_values(by=['Speed', 'ROADCLASS'], ascending=False)
+
+    # write the svg!
     svg_string = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1" fill="none">'
-    geo_df = geo_df.sort_values(by=['Speed'], ascending=False) # place gray roads below green roads
     for row in geo_df.iterfeatures():
         svg_string += row['properties']['svg'] + '\n'
     svg_string += '</svg>'
@@ -58,8 +66,20 @@ def row_to_svg(row):
         svg_line += f'{x},{y} L'
     svg_line = svg_line[:-2]
 
-    color = 'green' if row['Speed'] <= 25 else '#d8d8d8'
-    width = 0.0015 if row['Speed'] <= 25 else 0.0040
+    color = '#eee'
+    width = 0.0015
+
+    if row['Speed'] <= 25:
+        color = 'green'
+    else:
+        width = 0.0050
+
+    if row['ROADCLASS'] in ['Highway', 'Major Arterial']:
+        color = '#ddd'
+        width = 0.0060
+
+    if row['ROADCLASS'] in ['Ramp']:
+        color = '#eee'
 
     return f'<path stroke="{color}" stroke-width="{width}" d="{svg_line}"/>'
 
